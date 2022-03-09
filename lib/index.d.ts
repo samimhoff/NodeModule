f139185563bab468aa5f3f013b775ce2b1348bdb
Sam Imhoff <simhoff@costar.com>
2022-03-09 10:18:05 -0500

    changed version to 1.1.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,59 @@
+import { WeaponTypes } from "./weapons/weaponTypes";
 export interface IWeapon {
     name: string;
-    rateOfFire: number;
-    countryOfOrigin: string;
+    action: action;
+    caliber: string | caliber;
+    nationOfOrigin: nation;
+    effectiveRange: number;
+    rateOfFire?: number;
+    weight: number;
+    capacity: number;
+    year: Date;
+    description: string;
+    weaponType: WeaponTypes;
+    crewServed: boolean;
 }
-export declare function isEven(num: number): boolean;
+export declare enum targets {
+    personelle = 0,
+    armored = 1
+}
+export declare enum caliber {
+    NineMM = "9mm",
+    FortyFiveACP = ".45ACP",
+    EightMMMauser = "8mm Mauser"
+}
+export declare enum action {
+    manual = "manual",
+    semiAutomatic = "semi automatic",
+    automatic = "automatic"
+}
+export declare enum nation {
+    france = "France",
+    germany = "Germany",
+    unitedStates = "United States",
+    unitedKingdom = "United Kingdom",
+    sovietUnion = "Soviet Union",
+    japan = "Japan",
+    poland = "Poland",
+    china = "China",
+    italy = "Italy",
+    vichyFrance = "Vichy France",
+    romania = "Romania",
+    finland = "Finland",
+    india = "India",
+    norway = "Norway"
+}
+export declare enum Faction {
+    allies = "Allies",
+    axis = "Axis",
+    commintern = "Commintern"
+}
+export interface INation {
+    name: string;
+    faction: Faction;
+    description?: string;
+}
+export declare const Nations: {
+    [name: string]: INation;
+};
 //# sourceMappingURL=index.d.ts.map
